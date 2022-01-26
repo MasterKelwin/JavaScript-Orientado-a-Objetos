c14fd316628d6e78b66b9161a89405d0cd0fc32c
MasterKelwin <mkelwin@outlook.com>
2022-01-26 11:29:39 -0300

    Criando métodos

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 class cliente{ //molde
     nome; //atributos do cliente
     cpf;
-    agencia;
-    saldo;
     rg;
+}
+
+class contaCorrente{
+    saldo;
+    agencia;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+    
+    depositar(valor){
+        this.saldo += valor
+    }
 }
 
 const cliente1 = new cliente();  //objeto
 cliente1.nome = "Kelwin";
 cliente1.cpf = 11122233344;
-cliente1.agencia = 0785;
-cliente1.saldo = 0;
-cliente1.rg = 123456789
+cliente1.rg = 123456789; 
+const contaCorrenteKelwin = new contaCorrente();
+contaCorrenteKelwin.saldo = 0;
+contaCorrenteKelwin.agencia = 0712;
+
+contaCorrenteKelwin.depositar(150);
+contaCorrenteKelwin.sacar(50);
 
 const cliente2 = new cliente(); //objeto
 cliente2.nome = "Dara";
 cliente2.cpf = 99988877766;
-cliente2.agencia = 0785;
-cliente2.saldo = 0;
+cliente2.rg = 987654321;
+const contaCorrenteDara = new contaCorrente();
+contaCorrenteDara.saldo = 0;
+contaCorrenteDara.agencia = 0712; 
 
-console.log(cliente1,cliente2);
+contaCorrenteDara.depositar(200);
+contaCorrenteDara.sacar(150);
+
+//console.log(cliente1,cliente2);
+console.log(contaCorrenteKelwin.saldo,contaCorrenteDara.saldo);
 
 
