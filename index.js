a4aab304ff341f0c9bb38e6f9296c31cad43bc6a
MasterKelwin <mkelwin@outlook.com>
2022-01-26 12:10:49 -0300

    Atributos privados

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ class cliente{ //molde
 }
 
 class contaCorrente{
-    saldo;
+    _saldo;
     agencia;
 
     sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this._saldo >= valor && valor >= 0){
+            this._saldo -= valor;
         }
     }
     
     depositar(valor){
-        this.saldo += valor
+        if(valor > 0){
+        this._saldo += valor;
+        }
     }
 }
 
@@ -24,24 +26,26 @@ cliente1.nome = "Kelwin";
 cliente1.cpf = 11122233344;
 cliente1.rg = 123456789; 
 const contaCorrenteKelwin = new contaCorrente();
-contaCorrenteKelwin.saldo = 0;
+contaCorrenteKelwin._saldo = 0;
 contaCorrenteKelwin.agencia = 0712;
 
 contaCorrenteKelwin.depositar(150);
 contaCorrenteKelwin.sacar(50);
+// contaCorrenteKelwin.depositar(-2);
+// contaCorrenteKelwin.sacar(-2);
 
 const cliente2 = new cliente(); //objeto
 cliente2.nome = "Dara";
 cliente2.cpf = 99988877766;
 cliente2.rg = 987654321;
 const contaCorrenteDara = new contaCorrente();
-contaCorrenteDara.saldo = 0;
+contaCorrenteDara._saldo = 0;
 contaCorrenteDara.agencia = 0712; 
 
 contaCorrenteDara.depositar(200);
 contaCorrenteDara.sacar(150);
 
 //console.log(cliente1,cliente2);
-console.log(contaCorrenteKelwin.saldo,contaCorrenteDara.saldo);
+console.log(contaCorrenteKelwin._saldo,contaCorrenteDara._saldo);
 
 
